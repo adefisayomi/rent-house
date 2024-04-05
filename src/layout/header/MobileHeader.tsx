6df8bd5e6ca7ef6764aa347cee9eb4fcda943c72
dolapo oluwole <adefisayomioluwole@gmail.com>
2024-04-06 00:13:07 +0100

    updated the mobile header

--- src/layout/header/MobileHeader.tsx
+++ src/layout/header/MobileHeader.tsx
@@ -1,11 +1,17 @@
+import Logo from "@/components/Logo";
+import { Menu } from "lucide-react";
 
 
 
 export default function MobileHeader () {
 
     return (
-        <div>
-            Mobile Header
+        <div className="w-full mx-auto max-w-9xl flex items-center justify-between p-2 h-24">
+            <Logo/>
+
+            <div className="flex items-center gap-4">
+                <Menu />
+            </div>
         </div>
     )
 }
